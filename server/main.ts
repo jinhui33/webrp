2df215939d0e57a1bec50062e54f086baa3fc307
A-yon Lee <i@hyurl.com>
2025-02-05 04:13:11 +0800

    log Deno info

--- server/main.ts
+++ server/main.ts
@@ -4,7 +4,7 @@ import app from "./app.ts"
 export default serve({
     type: "module",
     fetch: (req, ctx) => {
-        console.log(ctx, typeof Deno, Deno.version.deno)
+        console.log(ctx, typeof Deno, Deno.version)
         return app.fetch(req, ctx)
     },
 })
