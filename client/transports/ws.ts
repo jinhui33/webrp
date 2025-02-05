7c1069dcab83bc81c8aa731d37f0027f78393bcf
A-yon Lee <i@hyurl.com>
2025-02-05 12:14:35 +0800

    fix reconnection for ws transport

--- client/transports/ws.ts
+++ client/transports/ws.ts
@@ -3,18 +3,24 @@ import { parseProxyRequest } from "../../common.ts"
 import { getConfig } from "../util.ts"
 import chat from "../chat.ts"
 
+let connectedBefore = false
+
 export default function wsConnect() {
     const { serverUrl, agentId } = getConfig()
     const ws = new WebSocket(serverUrl + "/ws?agentId=" + agentId)
 
     ws.addEventListener("open", () => {
+        connectedBefore = true
         console.log("Connected to the server")
     })
 
     ws.addEventListener("error", () => {
-        if (ws.readyState === WebSocket.CONNECTING) {
+        if (ws.readyState === WebSocket.CONNECTING ||
+            (ws.readyState === WebSocket.CLOSED && !connectedBefore)
+        ) {
             console.log("Failed to connect to the server, will retry in 5 seconds")
             setTimeout(() => {
+                connectedBefore = false
                 console.log("Reconnecting...")
                 wsConnect()
             }, 5000)
@@ -22,11 +28,14 @@ export default function wsConnect() {
     })
 
     ws.addEventListener("close", () => {
-        console.log("Disconnected from the server")
-        setTimeout(() => {
-            console.log("Reconnecting...")
-            wsConnect()
-        }, 0)
+        if (connectedBefore) {
+            console.log("Disconnected from the server")
+            setTimeout(() => {
+                connectedBefore = false
+                console.log("Reconnecting...")
+                wsConnect()
+            }, 0)
+        }
     })
 
     ws.addEventListener("message", event => {
