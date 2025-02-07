8f89ee037286e752fc562fa0afbd6d117337d539
A-yon Lee <i@hyurl.com>
2025-02-07 12:04:04 +0800

    improve Node.js support

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,12 @@
 import "../init.ts"
 import { serve } from "@ayonli/jsext/http"
+import runtime from "@ayonli/jsext/runtime"
 import app from "./app.ts"
 
+const isNode = runtime().identity === "node"
+const isNodeESEntry = process.execArgv.some(arg => arg.endsWith("@ayonli/jsext/http"))
+
 export default serve({
-    type: "module",
+    type: !isNode || isNodeESEntry ? "module" : "classic",
     fetch: app.fetch,
 })
