1c363e4d4cf3057c368401f14d721e29bb02c922
ayuan.li <ayuan.li@narwal.com>
2025-02-07 19:24:23 +0800

    Revert "Revert "add FORCE_CORS setting""
    This reverts commit fdd460f54c2a65f087d041c6a0de3c664a363000.

--- server/app.ts
+++ server/app.ts
@@ -273,6 +273,11 @@ const app = new Hono<{ Bindings: any }>()
             headers.set("host", host)
         }
 
+        const { FORCE_CORS } = env()
+        if (FORCE_CORS?.toLowerCase().match(/^(true|on|1)$/)) {
+            headers.delete("origin")
+        }
+
         const header = {
             requestId,
             type: "header",
